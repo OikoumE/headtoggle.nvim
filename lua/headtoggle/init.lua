0e2bee90bf24de9e927daf1b2f38dafc4b3e7527
OikoumE <oikoume@gmail.com>
2025-11-11 21:30:08 +0100

    added file exsist check

--- lua/headtoggle/init.lua
+++ lua/headtoggle/init.lua
@@ -1,4 +1,9 @@
 local M = {}
+M.file_exsist = function(target)
+	local exsist = vim.fn.filereadable(target) == 1
+	return exsist
+end
+
 M.switch_file = function(split)
 	-- local file_name = vim.fn.expand("%:t")
 	-- local file_type = vim.bo.filetype
@@ -13,25 +18,31 @@ M.switch_file = function(split)
 		vim.notify("not a h/cpp file", vim.diagnostic.severity.WARN)
 		return
 	end
-	if split == "v" then
-		vim.cmd("vsplit " .. target)
-	elseif split == "h" then
-		vim.cmd("split " .. target)
+	if M.file_exsist(target) then
+		if split == "v" then
+			vim.cmd("vsplit " .. target)
+		elseif split == "h" then
+			vim.cmd("split " .. target)
+		else
+			vim.cmd("edit " .. target)
+		end
 	else
-		vim.cmd("edit " .. target)
+		if file_ext == "h" then
+			vim.notify("no .cpp file found for: " .. file_name_no_ext)
+		elseif file_ext == "cpp" then
+			vim.notify("no .h file found for: " .. file_name_no_ext)
+		end
 	end
 end
 
 M.setup = function()
-	print("headtoggle")
-	vim.notify("hiiiiii", vim.diagnostic.severity.ERROR)
 	vim.keymap.set("n", "<leader>cT", function()
 		require("headtoggle").switch_file()
 	end, { desc = "code [T]oggle .h/.cpp" })
-	vim.keymap.set("n", "<leader>cV", function()
+	vim.keymap.set("n", "<leader>cv", function()
 		require("headtoggle").switch_file("v")
 	end, { desc = "code open .h/.cpp vertical split" })
-	vim.keymap.set("n", "<leader>cv", function()
+	vim.keymap.set("n", "<leader>cV", function()
 		require("headtoggle").switch_file("h")
 	end, { desc = "code open .h/.cpp horizontal spli" })
 end
